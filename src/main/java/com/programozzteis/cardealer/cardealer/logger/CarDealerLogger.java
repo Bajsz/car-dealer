76031ea872305c1d648521b9b5826d5d254d8c8a
Bajsz <bajortamas@hotmail.com>
2020-02-27 15:04:10 +0100

    Comment section added

--- src/main/java/com/programozzteis/cardealer/cardealer/logger/CarDealerLogger.java
+++ src/main/java/com/programozzteis/cardealer/cardealer/logger/CarDealerLogger.java
@@ -3,7 +3,11 @@ package com.programozzteis.cardealer.cardealer.logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
+/**
+ * Wrapper for the Logger: One common Logger is enough for the Application
+ * @author Bajor
+ *
+ */
 public class CarDealerLogger {
 
 	private static final Logger logger = LogManager.getLogger(CarDealerLogger.class);
